7bd8ca962fe49af9905f7ffb8805c3521b289320
Gabriel Martin <gabrielmartin13009@gmail.com>
2026-03-04 10:01:50 +0100

    feat(history): enhance history view with user-specific scan data and improved UI

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\ScanJobRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HistoryController extends AbstractController
 {
     #[Route('/history', name: 'app_history')]
-    public function index(): Response
+    #[IsGranted('ROLE_USER')]
+    public function index(ScanJobRepository $scanJobRepository): Response
     {
-        return $this->render('history/index.html.twig');
+        $user = $this->getUser();
+        $scanJobs = $scanJobRepository->findBy(
+            ['user' => $user],
+            ['createdAt' => 'DESC']
+        );
+
+        return $this->render('history/index.html.twig', [
+            'scanJobs' => $scanJobs,
+        ]);
     }
 }
