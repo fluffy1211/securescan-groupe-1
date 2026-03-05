4299a61d51e6608ca4e02d475519299632d4f29e
Ayman <aymoug@outlook.fr>
2026-03-05 14:55:57 +0100

    Gestion des migrations

--- migrations/Version20260305135200.php
+++ migrations/Version20260305135200.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260305132155 extends AbstractMigration
+final class Version20260305135200 extends AbstractMigration
 {
     public function getDescription(): string
     {
